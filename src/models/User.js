a784ab5afd5423e0af8525678763e30b4f81371e
Manoj Patel BM <manojpatelbm182005@gmail.com>
2026-02-12 19:00:27 +0530

    todays

--- src/models/User.js
+++ src/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     profilePicture: { type: String, default: '' }, 
 }, { timestamps: true });
 
-// Middleware to hash password before saving
+
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
